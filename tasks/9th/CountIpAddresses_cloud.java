1dbc698d8097286b0cc385094d227177b35fcae3
bluecloud <bluecloud0429@gmail.com>
2019-06-10 21:47:04 +0900

    9th_codewars test class add

--- tasks/9th/CountIpAddresses_cloud.java
+++ tasks/9th/CountIpAddresses_cloud.java
@@ -35,3 +35,29 @@ public class CountIPAddresses {
         return result;
     }
 }
+
+// test class
+import static org.junit.Assert.assertEquals;
+
+import codewars.CountIPAddresses;
+import com.sun.org.apache.xpath.internal.functions.WrongNumberArgsException;
+import org.junit.Test;
+
+public class CountIPAddressesTest {
+
+    @Test
+    public void normalTest() throws Exception {
+        assertEquals( 50, CountIPAddresses.ipsBetween( "10.0.0.0", "10.0.0.50" ) );
+        assertEquals( 246, CountIPAddresses.ipsBetween( "20.0.0.10", "20.0.1.0" ) );
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void paramIsNumber() throws Exception {
+        CountIPAddresses.ipsBetween( "a", "b" );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void endIsBigger() throws Exception {
+        CountIPAddresses.ipsBetween( "0.0.0.1", "0.0.0.0" );
+    }
+}
